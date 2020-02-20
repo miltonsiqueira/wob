909d0288ad0818b778a5a51f89f26e73ca4fa0d0
jacksonjjfjr <jackson.jesus@gmail.com>
2020-02-20 16:27:44 -0400

    Base screen basic styling

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import ItemRow from './components/ItemRow'
 
 function App() {
   return (
-    <div className="App">
-      <h1>WOB</h1>
-        <ItemRow index = {1}/>
-        <ItemRow index = {2}/>
-      <div>
-        <button>Compare</button>
+    <div className="container">
+      <div className="App">
+        <h1>WOB</h1>
+          <ItemRow index = {1}/>
+          <ItemRow index = {2}/>
+        <div>
+          <button>Compare</button>
+        </div>
       </div>
     </div>
+    
   );
 }
 
