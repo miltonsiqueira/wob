84fa226461f14ad8175f901c18aeba6e17598206
jacksonjjfjr <jackson.jesus@gmail.com>
2020-02-20 15:50:47 -0400

    ItemRow Components

--- src/App.js
+++ src/App.js
@@ -1,27 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ItemRow from './components/ItemRow'
 
 function App() {
   return (
     <div className="App">
       <h1>WOB</h1>
-      <div>
-        <span>Item 1</span>
-        <select>
-          <option>kg</option>
-          <option>g</option>
-        </select>
-        <input type="number" placeholder="value" />
-      </div>
-      <div>
-        <span>Item 2</span>
-        <select>
-          <option>kg</option>
-          <option>g</option>
-        </select>
-        <input type="number" placeholder="value" />
-      </div>
+        <ItemRow index = {1}/>
+        <ItemRow index = {2}/>
       <div>
         <button>Compare</button>
       </div>
