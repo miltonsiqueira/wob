123cc10f89954f0351b1b33fbcdfc358b358664b
jacksonjjfjr <jackson.jesus@gmail.com>
2020-02-20 15:25:43 -0400

    Is it finally right

--- src/App.js
+++ src/App.js
@@ -5,20 +5,26 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>WOB</h1>
+      <div>
+        <span>Item 1</span>
+        <select>
+          <option>kg</option>
+          <option>g</option>
+        </select>
+        <input type="number" placeholder="value" />
+      </div>
+      <div>
+        <span>Item 2</span>
+        <select>
+          <option>kg</option>
+          <option>g</option>
+        </select>
+        <input type="number" placeholder="value" />
+      </div>
+      <div>
+        <button>Compare</button>
+      </div>
     </div>
   );
 }
